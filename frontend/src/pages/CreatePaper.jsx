e7066b5322a242d1b4c36cdceba98e2d7c16d3bc
Shiwangi Kumari <sshiwangi770@gmail.com>
2023-11-24 11:13:34 +0530

    modified createPaper page

--- frontend/src/pages/CreatePaper.jsx
+++ frontend/src/pages/CreatePaper.jsx
@@ -2,6 +2,10 @@ import React, { useState } from "react";
 import Input from "../components/Input";
 // import Select from '../components/Select'
 
+function classNames(...classes) {
+  return classes.filter(Boolean).join(' ')
+}
+
 function CreatePaper() {
   const initialState = {
     totalMarks: 0,
@@ -44,18 +48,38 @@ const [selectedQuestions, setSelectedQuestions] = useState([]);
   };
 
   return (
-    <form onSubmit={handleSubmit}>
+    <div className="isolate bg-white px-6 py-24 sm:py-32 lg:px-8">
+      <div
+        className="absolute inset-x-0 top-[-10rem] -z-10 transform-gpu overflow-hidden blur-3xl sm:top-[-20rem]"
+        aria-hidden="true"
+      >
+        <div
+          className="relative left-1/2 -z-10 aspect-[1155/678] w-[36.125rem] max-w-none -translate-x-1/2 rotate-[30deg] bg-gradient-to-tr from-[#e5e0e2] to-[#c2edef] opacity-30 sm:left-[calc(50%-40rem)] sm:w-[72.1875rem]"
+          style={{
+            clipPath:
+              'polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)',
+          }}
+        />
+      </div>
+      <div className="mx-auto max-w-2xl text-center">
+        <h2 className="text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">Create Paper</h2>
+        <p className="mt-2 text-lg leading-8 text-gray-600">
+           Craft personalized question papers effortlessly by providing these
+            details.
+        </p>
+      </div>
+    <form onSubmit={handleSubmit} className="mx-auto mt-16 max-w-xl sm:mt-20">
       <div className="space-y-12">
         <div className="border-b border-gray-900/10 pb-12">
-          <h2 className="text-base font-semibold leading-7 text-gray-900">
+          {/* <h2 className="text-base font-semibold leading-7 text-gray-900">
             Create Paper
           </h2>
           <p className="mt-1 text-sm leading-6 text-gray-600">
             Craft personalized question papers effortlessly by providing these
             details.
-          </p>
+          </p> */}
 
-          <div className="mt-10 grid grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6">
+          <div className="mt-10">
             <div className="sm:col-span-4">
               <Input
                 for="total-marks"
@@ -71,7 +95,7 @@ const [selectedQuestions, setSelectedQuestions] = useState([]);
               />
             </div>
           </div>
-          <div className="mt-10 grid grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6">
+          <div className="mt-10">
             <div className="sm:col-span-4">
               {/* <Select onBasisChange={(basis) => handleInputChange('basis', basis)}/> */}
               <Input
@@ -89,7 +113,7 @@ const [selectedQuestions, setSelectedQuestions] = useState([]);
             </div>
           </div>
 
-          <div className="mt-10 grid grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6">
+          <div className="mt-10">
             <div className="sm:col-span-4">
               <Input
                 for="medium"
@@ -105,7 +129,7 @@ const [selectedQuestions, setSelectedQuestions] = useState([]);
               />
             </div>
           </div>
-          <div className="mt-10 grid grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6">
+          <div className="mt-10">
             <div className="sm:col-span-4">
               <Input
                 for="hard"
@@ -124,13 +148,13 @@ const [selectedQuestions, setSelectedQuestions] = useState([]);
         </div>
       </div>
 
-      <div className="mt-6 flex items-center justify-start gap-x-6">
+      <div className="mt-6 flex items-center justify-center gap-x-6">
         {/* <button type="button" className="text-sm font-semibold leading-6 text-gray-900">
           Cancel
         </button> */}
         <button
           type="submit"
-          className="rounded-md bg-reelobg px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-reelobg focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-reelobg"
+          className="w-full rounded-md bg-reelobg px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-reelobg focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-reelobg"
         >
           Generate
         </button>
@@ -147,6 +171,7 @@ const [selectedQuestions, setSelectedQuestions] = useState([]);
         </div>
       )}
     </form>
+    </div>
   );
 }
 
